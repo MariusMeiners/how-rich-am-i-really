0460055cb7d3d75182525c2d5366bc14606f24dc
mariusmeiners <marius@meiners.dev>
2021-05-29 11:18:11 +0200

    Put country in sidebar

--- app.py
+++ app.py
@@ -3,17 +3,26 @@ import streamlit as st
 # working with sample data.
 import numpy as np
 import pandas as pd
+import pycountry
 
 st.title('How rich are you really?')
 
 st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
+
+df = pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [100000, 20, 30, 40]
-}))
+})
+st.write(df)
 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+
+option = st.sidebar.selectbox(
+    'Which country do you live in?',
+     [x.name for x in list(pycountry.countries)])
+
+'You selected:', option
